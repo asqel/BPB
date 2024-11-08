04e78d2f2b95047016b008d127e0e58f52b70995
pf4 <74263128+elydre@users.noreply.github.com>
2024-11-08 20:20:03 +0100

    split into multiple files

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,75 +1,4 @@
-
 #include <kernel.h>
-void serial_putc(char c) {
-    while (!(port_read_u8(SERIAL_PORT + 5) & 0x20));
-    port_write_u8(0x3f8, c);
-}
-void serial_putstr(char *s) {
-    while (*s)
-        serial_putc(*(s++));
-}
-// int seed = 0;
-
-
-void serial_putnbr(int n) {
-    if (n ==  (int)0x80000000) {
-        serial_putstr("-2147483648");
-        return ;
-    }
-    if (n < 0) {
-        serial_putc('-');
-        serial_putnbr(-n);
-        return ;
-    }
-    if (n == 0) {
-        serial_putc('0');
-        return;
-    }
-    if (n < 10) {
-        serial_putc('0' + n);
-        return;
-    }
-    serial_putnbr(n / 10);
-    serial_putnbr(n % 10);
-}
-
-
-void screen_puts(char *string) {
-    for (int i = 0; string[i] != 0; i++) {
-        screen_add_char(string[i], 0x0f);
-    }
-}
-
-// int random() {
-//     unsigned int next = seed;
-//     int result;
-
-//     next *= 1103515245;
-//     next += 12345;
-//     result = (unsigned int) (next / 65536) % 2048;
-
-//     next *= 1103515245;
-//     next += 12345;
-//     result <<= 10;
-//     result ^= (unsigned int) (next / 65536) % 1024;
-
-//     next *= 1103515245;
-//     next += 12345;
-//     result <<= 10;
-//     result ^= (unsigned int) (next / 65536) % 1024;
-
-//     seed = next;
-//     return result;
-// }
-
-char keyboard_map[] = {
-	0, 0, '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '!', '=', '\b',
-	'\t', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '(', ')',
-	'\n', 0, 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', '`',
-	0, '\\', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 0,
-	'*', 0, ' ', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '7', '8', '9',
-	'-', '4', '5', '6', '+', '1', '2', '3', '0', '.'
-};
 
 void close_os() {
     port_write_u16(0x604, 0x2000);   // qemu
@@ -80,77 +9,13 @@ void close_os() {
     asm volatile("hlt");
 }
 
-int python_style_input(char *buffer, int max) {
-    unsigned char c, old = 0;
-    int current = 0;
-    buffer[0] = 0;
-
-    while (port_read_u8(0x60) == 0x1c);
-
-    while (1) {
-        c = port_read_u8(0x60);
-        if (c == old)
-            continue;
-        old = c;
-
-        if (c == 0x01)
-            close_os();
-
-        if (c == 0x1c) {
-            screen_add_char('\n', 0x0f);
-            return current;
-        }
-
-        if (c == 0x0e) {
-            if (current > 0) {
-                screen_erase();
-                current--;
-                buffer[current] = 0;
-            }
-            continue;
-        }
-        
-        if (c < 128 && current < max - 1) {
-            buffer[current] = keyboard_map[c];
-            if (buffer[current] == 0)
-                continue;
-            screen_add_char(buffer[current], 0x0A);
-            buffer[++current] = 0;
-        }
-    }
-}
-
-// void interpret(char *buffer) {
-//     if (!*buffer) {
-//         return;
-//     }
-//     if (strcmp(buffer, "exit") == 0) {
-//         close_os();
-//     } else if (strcmp(buffer, "clear") == 0) {
-//         screen_clear();
-//     } else {
-//         screen_puts("Invalid command: ");
-//         screen_puts(buffer);
-//         screen_puts("\n");
-//     }
-// }
-
-void serial_init() {
-    port_write_u8(SERIAL_PORT + 1, 0x00);
-    port_write_u8(SERIAL_PORT + 3, 0x80); 
-    port_write_u8(SERIAL_PORT + 0, 0x03);
-    port_write_u8(SERIAL_PORT + 1, 0x00); 
-    port_write_u8(SERIAL_PORT + 3, 0x03);
-    port_write_u8(SERIAL_PORT + 2, 0xC7);
-    port_write_u8(SERIAL_PORT + 4, 0x0B);
-}
-
 int olivine_main(int argc, char **argv);
+int puts(const char *s);
 
 void kernel_main() {
-	serial_init();
     screen_clear();
-	serial_putstr("REAL KERNEL\n");
+    puts("it's a good idea to want to make an os that runs Windows exe and graphic driverslike to be able to run games (._.  )\n");
+	serial_init();
     olivine_main(1, (char *[]){"olivine"});
 	while (1);
 }
